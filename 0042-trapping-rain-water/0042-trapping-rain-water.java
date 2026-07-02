2f2ce1f66496f9b8f59e7df0834dc7a871bee680
Ashutosh Vyas <ashutosh.vyas2504@gmail.com>
2026-07-02 11:08:50 +0530

    Time: 0 ms (100%), Space: 47 MB (99.25%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int trap(int[] height) {
+        // Using 2 Pointers
+        int left = 0; 
+        int right = height.length-1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int ans = 0;
+
+        while(left<right){
+
+        if(height[left]<=height[right]){  
+            if(height[left]>=leftMax){
+                leftMax = height[left];
+            } else {
+                ans = ans + (leftMax-height[left]);
+            }
+            left++;
+        } else {
+            if(height[right]>=rightMax){
+                rightMax = height[right];
+            } else {
+                ans = ans +(rightMax - height[right]);
+            }
+            right--;
+        }
+     }
+     return ans;
+  }
+}
