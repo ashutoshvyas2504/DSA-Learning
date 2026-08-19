6783e308ab0a380d56cc4b63a103381d0d9dccd5
Ashutosh Vyas <ashutosh.vyas2504@gmail.com>
2026-08-19 10:24:32 +0530

    Time: 0 ms (100%), Space: 42.1 MB (44.76%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int temp = x;
+        int sum = 0;
+
+        while(temp!=0){
+            int digit = temp % 10;
+            sum = sum + digit;
+            temp = temp / 10;
+        }
+
+        if(x % sum == 0){
+            return sum;
+        } else {
+           return -1;
+        }
+    }
+}
