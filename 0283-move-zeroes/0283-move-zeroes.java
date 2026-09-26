a9ef191c921394db5b922a5ea1474110d8b271ee
Ashutosh Vyas <ashutosh.vyas2504@gmail.com>
2026-09-27 00:00:47 +0530

    Time: 2 ms (91.58%), Space: 48 MB (20.26%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -4,14 +4,15 @@ class Solution {
         int j = 0;
 
         for (int i = 0; i < nums.length; i++) {
-               
-              if(nums[i]!=0){
+            // Here i is searching for non zero element  
+            if (nums[i] != 0) {
+            // j tells where the non Zero element belongs
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
 
                 j++;
-              }
+            }
         }
     }
 }
